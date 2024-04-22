1fea92db4d348ed70b524fcc366540fe0176f437
Ebenezer Isaac <mail@ebenezer-isaac.com>
2024-04-22 22:02:15 +0530

    updated action file

--- environment_variables.php
+++ environment_variables.php
@@ -2,18 +2,23 @@
 
 // Accessing the MongoDB connection details
 $mongoDbConnectionString = getenv('MONGO_DB_CONNECTION_STRING');
-$mongoDbName = getenv('MONGO_DB_NAME');
 $mongoDbCollectionName = getenv('MONGO_DB_COLLECTION_NAME');
+$mongoDbName = getenv('MONGO_DB_NAME');
 
 // Accessing the GitHub details
-$githubUsername = getenv('GITHUB_USERNAME');
-$githubPat = getenv('GITHUB_PAT');
+$githubUsername = getenv('GH_USERNAME');
+$githubPat = getenv('GH_PAT');
+
 
 // Example usage
 echo "MongoDB Connection String: $mongoDbConnectionString\n";
+echo "<br>";
 echo "MongoDB Name: $mongoDbName\n";
+echo "<br>";
 echo "MongoDB Collection Name: $mongoDbCollectionName\n";
+echo "<br>";
 echo "GitHub Username: $githubUsername\n";
+echo "<br>";
 echo "GitHub PAT: $githubPat\n";
 
 // Remember to replace `echo` statements with your actual logic that uses these variables.
